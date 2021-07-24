2590ba87d397b166b587ab0470236b1c475e5c54
Kenneth Christensen <kenbochr@gmail.com>
2021-07-24 12:35:22 +0200

    RunOnStartup

--- TimerTrigger.cs
+++ TimerTrigger.cs
@@ -9,8 +9,8 @@ namespace DaluxFM
 {
     public static class TimerTrigger
     {
-        [Function("TimerTrigger5")]
-        public static void Run([TimerTrigger("0 0 1 * * *")] MyInfo myTimer, FunctionContext context)
+        [Function("TimerTrigger6")]
+        public static void Run([TimerTrigger("%ScheduleExpression%", RunOnStartup = true)] MyInfo myTimer, FunctionContext context)
         {
             var logger = context.GetLogger("Function1");
             logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
